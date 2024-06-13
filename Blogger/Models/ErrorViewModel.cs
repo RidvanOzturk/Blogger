4684acbb6180c51cb346b092436132ac4838d2e8
RidvanOzturk <ozturkridvan1@gmail.com>
2024-06-13 09:53:42 +0300

    Add project files.

---
+++ Blogger/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Blogger.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
