013365f72c5c770d2e1c925b16165eb3a2f4970c
RidvanOzturk <ozturkridvan1@gmail.com>
2024-06-24 10:39:31 +0300

    adding users to the database was completed

--- Blogger/Controllers/AccountController.cs
+++ Blogger/Controllers/AccountController.cs
@@ -1,16 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blogger.Data;
+using Blogger.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace Blogger.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly BlogContext _context;
+
+        public AccountController(BlogContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            var user = _context.Users.SingleOrDefault(u => u.Username == username && u.Password == password);
+            if (user != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.Error = "Wrong Username or Password";
+            return View();
+        }
+
+        [HttpGet]
         public IActionResult SignUp()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(string username, string password)
+        {
+            var newUser = new User { Username = username, Password = password };
+            _context.Users.Add(newUser);
+            _context.SaveChanges();
+
+            return RedirectToAction("Login");
         }
     }
 }
