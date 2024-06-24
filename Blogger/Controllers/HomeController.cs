fd0a8bb1e72de8de313a2b89477322b7ce523822
RidvanOzturk <ozturkridvan1@gmail.com>
2024-06-24 14:40:40 +0300

    edited authorize operations.

--- Blogger/Controllers/HomeController.cs
+++ Blogger/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 using Blogger.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace Blogger.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -22,7 +24,6 @@ namespace Blogger.Controllers
         {
             return View();
         }
-      
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
