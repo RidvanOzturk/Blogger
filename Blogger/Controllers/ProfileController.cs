7f54dfc9aced069f6bfe383f56fb29b90c35a34a
RidvanOzturk <ozturkridvan1@gmail.com>
2024-07-05 11:15:21 +0300

    edited Welcome View and added Profile Page

---
+++ Blogger/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blogger.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
