a40830355946ead4777be1c18ab732c1ecbb6da8
RidvanOzturk <ozturkridvan1@gmail.com>
2024-07-09 10:04:31 +0300

    changes bugs will be solved

--- Blogger/Controllers/ProfileController.cs
+++ Blogger/Controllers/ProfileController.cs
@@ -1,4 +1,7 @@
-﻿using Blogger.Data;
+﻿using BCrypt.Net;
+using Blogger.Data;
+using Blogger.Entities;
+using Blogger.Requests;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,26 +14,84 @@ namespace Blogger.Controllers
         {
             _context = context;
         }
-        
+
         public IActionResult ProfileDetail(int id)
         {
             var user = _context.Users
-                 .Include(u => u.Posts)
-                 .Include(u => u.Comments)
-                 .FirstOrDefault(u => u.Id == id);
+                .Include(u => u.Posts)
+                .Include(u => u.Comments)
+                .FirstOrDefault(u => u.Id == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
 
             return View(user);
         }
 
-        [HttpPost]
-        public IActionResult ChangeUrId(int id)
+        [HttpGet]
+        public IActionResult ChangeUsername(int id)
         {
             var user = _context.Users.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
         }
+
         [HttpPost]
-        public IActionResult ChangeUrPassword(int id)
+        public async Task<IActionResult> ChangeUsername(User model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _context.Users.FindAsync(model.Id);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+
+                user.Username = model.Username;
+
+                _context.Update(user);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("ProfileDetail", new { id = model.Id });
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult ChangePassword(int id)
         {
             var user = _context.Users.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(new ChangePasswordViewModel { Id = user.Id });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _context.Users.FindAsync(model.Id);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+
+                user.Password = BCrypt.Net.BCrypt.HashPassword(model.NewPassword);
+
+                _context.Update(user);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("ProfileDetail", new { id = model.Id });
+            }
+            return View(model);
         }
     }
 }
