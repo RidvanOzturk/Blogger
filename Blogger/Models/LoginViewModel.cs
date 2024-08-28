2fbda78feec148f16cadb7bcc6fe142b5daebc39
RidvanOzturk <ozturkridvan1@gmail.com>
2024-08-28 11:36:45 +0300

    login bug fixed

--- Blogger/Models/LoginViewModel.cs
+++ Blogger/Models/LoginViewModel.cs
@@ -4,15 +4,15 @@ namespace Blogger.Models
 {
     public class LoginViewModel
     {
-        [Required(ErrorMessage = "Username is required.")]
+        [Required]
         public string Username { get; set; }
 
-        [Required(ErrorMessage = "Password is required.")]
+        [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        public bool RememberMe { get; set; } 
-
-        public string ReturnUrl { get; set; } 
+        public bool RememberMe { get; set; }
     }
+
+
 }
